0325032489f0c8d2da49055a0d96c8fa8754f2e3
M0x1m <zhmyax01@gmail.com>
2024-06-07 00:35:00 +0300

    Implement simple HTTP parsing

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <time.h>
 #include <errno.h>
+#include <ctype.h>
 #include <stddef.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -42,6 +43,9 @@ typedef struct {
     size_t count;
 } String_View;
 
+#define SV_Fmt "%.*s"
+#define SV_Arg(sv) (int) sv.count, sv.data
+
 int sv_eq_cstr(String_View sv, const char *cstr)
 {
     size_t i;
@@ -59,6 +63,11 @@ String_View sv_from_parts(const char *data, size_t n)
     return sv;
 }
 
+String_View sv_from_cstr(const char *cstr)
+{
+    return sv_from_parts(cstr, strlen(cstr));
+}
+
 int sv_ends_with_cstr(String_View sv, const char *cstr)
 {
     const size_t cstr_len = strlen(cstr);
@@ -66,9 +75,91 @@ int sv_ends_with_cstr(String_View sv, const char *cstr)
             !sv_eq_cstr(sv_from_parts(&sv.data[sv.count-cstr_len], cstr_len), cstr);
 }
 
-String_View sv_from_sb(String_Builder *sb)
+String_View sv_from_sb(const String_Builder *sb)
 {
     return sv_from_parts(sb->items, sb->count);
+}
+
+String_View sv_chop(String_View *sv, size_t n)
+{
+    String_View chopped;
+    if (n > sv->count) n = sv->count;
+    chopped.data = sv->data;
+    chopped.count = n;
+    sv->data += n;
+    sv->count -= n;
+
+    return chopped;
+}
+
+String_View sv_chop_while(String_View *sv, int (*pred)(int))
+{
+    size_t i = 0;
+
+    while (i < sv->count && pred(sv->data[i])) {
+        i++;
+    }
+    return sv_chop(sv, i);
+}
+
+String_View sv_trim_right(String_View sv)
+{
+    size_t i = 0;
+
+    while (sv.count - i > 1 && isspace(sv.data[sv.count - i - 1])) {
+        ++i;
+    }
+
+    return sv_chop(&sv, sv.count - i);
+}
+
+String_View sv_trim_left(String_View sv)
+{
+    sv_chop_while(&sv, isspace);
+    return sv;
+}
+
+String_View sv_trim(String_View sv)
+{
+    return sv_trim_left(sv_trim_right(sv));
+}
+
+int sv_eq(String_View a, String_View b)
+{
+    size_t i;
+    if (a.count != b.count) return 0;
+
+    for (i = 0; i < a.count; ++i) {
+        if (a.data[i] != b.data[i]) return 0;
+    }
+
+    return 1;
+}
+
+String_View sv_chop_by_delim(String_View *sv, char delim)
+{
+    size_t i = 0;
+
+    while (i < sv->count && sv->data[i] != delim) {
+        ++i;
+    }
+
+    return sv_chop(sv, i);
+}
+
+String_View sv_chop_by_sv_delim(String_View *sv, String_View delim)
+{
+    size_t offset = 0;
+    const size_t limit = sv->count - delim.count;
+    while (offset < limit && !sv_eq(sv_from_parts(&sv->data[offset], delim.count), delim)) {
+        offset++;
+    }
+    return sv_chop(sv, offset + delim.count);
+}
+
+String_View sv_chop_by_cstr_delim(String_View *sv, const char *cstr)
+{
+    return sv_chop_by_sv_delim(sv, sv_from_cstr(cstr));
 }
 
 typedef struct {
@@ -96,9 +187,9 @@ int bs_fetch(BufStream *bs)
     int n;
     n = read(bs->fd, &bs->read_buf[bs->read_avail + bs->read_pos],
             sizeof(bs->read_buf) - bs->read_avail - bs->read_pos);
-    if (n < 0) {
+    if (n <= 0) {
         bs->error = (errno == EAGAIN || errno == EWOULDBLOCK)
-            ? 0 : errno;
+            ? n == 0 : errno;
         return -1;
     }
     bs->read_avail += n;
@@ -137,6 +228,28 @@ int read_until(BufStream *bs, String_Builder *sb, const char *until) {
     return 0;
 }
 
+typedef enum {
+    HTTP_METH_GET
+} HTTP_Method;
+
+int http_method_from_sv(String_View sv)
+{
+    if (sv_eq_cstr(sv, "GET")) return HTTP_METH_GET;
+    return -1;
+}
+
+typedef enum {
+    HTTP_CONN_KEEPALIVE,
+    HTTP_CONN_CLOSE
+} HTTP_Connection;
+
+int http_connection_from_sv(String_View sv)
+{
+    if (sv_eq_cstr(sv, "keep-alive")) return HTTP_CONN_KEEPALIVE;
+    if (sv_eq_cstr(sv, "close")) return HTTP_CONN_CLOSE;
+    return -1;
+}
+
 typedef struct {
     BufStream stream;
     bool dropped;
@@ -148,7 +261,9 @@ typedef struct {
     struct pollfd *pfd;
 
     String_Builder request;
-    String_View requested_path;
+    String_Builder requested_path;
+    String_View method;
+    String_View connection;
 
     int afd;
 } Connection;
@@ -182,6 +297,105 @@ Connection *find_connection_by_pfd(Connections *cons, struct pollfd *pfd)
     return NULL;
 }
 
+int ishex(int c)
+{
+    return ('0' <= c && c <= '9') ||
+        ('a' <= c && c <= 'f');
+}
+
+int from_hexchars(String_View chars, int *v)
+{
+    size_t i;
+    for (i = 0; i < chars.count; ++i) {
+        char c = tolower(chars.data[i]);
+        if (!ishex(c)) return -1;
+        *v |= (c > '9' ? c - 'a' : c - '0');
+    }
+    return 0;
+}
+
+String_Builder http_decode_path(String_View path, int *err)
+{
+    String_Builder sb = {0};
+
+    while (path.count) {
+        int b;
+        if (*path.data != '%') {
+            da_append(&sb, *sv_chop(&path, 1).data);
+            continue;
+        }
+
+        sv_chop(&path, 1);
+        if (path.count < 2 ||
+            from_hexchars(sv_chop(&path, 2), &b) < 0) {
+            goto error;
+        }
+    }
+
+    return sb;
+
+error:
+    free(sb.items);
+    memset(&sb, 0, sizeof(sb));
+    *err = 1;
+
+    return sb;
+}
+
+int parse_http_first_line(Connection *c, String_View line)
+{
+    int i;
+    for (i = 0; line.count; ++i) {
+        int err = 0;
+        String_View word = sv_chop_by_delim(&line, ' ');
+        line = sv_trim_left(line);
+        switch (i) {
+            case 0: c->method = word; break;
+            case 1:
+                    assert(!c->requested_path.items);
+                    c->requested_path = http_decode_path(word, &err);
+                    break;
+        }
+        if (err) return -1;
+    }
+    return 0;
+}
+
+int parse_http_line(Connection *c, String_View line, int ln)
+{
+    String_View header, value;
+
+    if (ln == 0) {
+        return parse_http_first_line(c, line);
+    }
+
+    header = sv_chop_by_delim(&line, ':');
+    sv_chop(&line, 1);
+    value = sv_trim(line);
+    if (value.count == 0) return -1;
+
+    /* TODO: make a place for headers which needs to 
+       be saved to make this place a simple loop */
+    if (sv_eq_cstr(header, "Connection")) {
+        c->connection = value;
+    }
+    return 0;
+}
+
+int parse_http_request(Connection *c)
+{
+    int linenumber;
+    String_View whole = sv_trim(sv_from_sb(&c->request));
+
+    for (linenumber = 0; whole.count; linenumber++) {
+        String_View line = sv_chop_by_cstr_delim(&whole, "\r\n");
+        line = sv_trim_right(line);
+        if (parse_http_line(c, line, linenumber) < 0) return -1;
+    }
+
+    return 0;
+}
+
 int client_request(Connection *c)
 {
     switch (read_until(&c->stream, &c->request, "\r\n\r\n")) {
@@ -190,20 +404,43 @@ int client_request(Connection *c)
         c->dropped = true;
         return -1;
     }
+
+    if (parse_http_request(c) < 0) {
+        c->dropped = true;
+        return -1;
+    }
+
+    printf(SV_Fmt"\n", SV_Arg(c->method));
+    printf(SV_Fmt"\n", SV_Arg(sv_from_sb(&c->requested_path)));
+    printf(SV_Fmt"\n", SV_Arg(c->connection));
+
     c->pfd->events = POLLOUT;
     c->state = CST_RESPONSING;
+
     return 0;
 }
 
-void client_response(Connection *c)
+int client_response(Connection *c)
 {
-    assert(0 && "Not implemented");
+    if (c->dropped) return -1;
+
+
+    return 0;
 }
 
 void handle_output(Connection *c)
 {
-    printf("request: %.*s\n", (int) c->request.count, c->request.items);
+    (void) c;
     assert(0 && "Not implemented");
+}
+
+void delete_connection(ServerCtx *ctx, Connection *c)
+{
+    close(c->stream.fd);
+    da_delete(&ctx->connections, c);
+    da_delete(&ctx->polls, c->pfd);
+    free(c->request.items);
+    free(c->requested_path.items);
 }
 
 void handle_input(ServerCtx *ctx, Connection *c)
@@ -211,12 +448,13 @@ void handle_input(ServerCtx *ctx, Connection *c)
     switch (c->state) {
     case CST_REQUEST: {
         if (client_request(c) < 0) {
-            da_delete(&ctx->connections, c);
-            da_delete(&ctx->polls, c->pfd);
+            delete_connection(ctx, c);
         }
     } break;
     case CST_RESPONSING: {
-        client_response(c);
+        if (client_response(c) < 0) {
+            delete_connection(ctx, c);
+        }
     } break;
     default: assert(0 && "unreachable");
     }
@@ -278,6 +516,7 @@ int main(int argc, char **argv)
     struct sockaddr_in addr;
     ServerCtx ctx;
 
+    (void) program_name;
     for (; arg; arg = shift_args(&argc, &argv)) {
         if (strcmp(arg, "-port") == 0) {
             if (argc == 0) {
